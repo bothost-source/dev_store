db6ca17f625fc179c5d2e95dac472b90cf95be47
bothost-source <destinyjob493@gmail.com>
2026-07-04 00:02:11 +0100

    Update app_colors.dart

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,41 +1,45 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  // Primary
   static const Color primary = Color(0xFF6C63FF);
   static const Color primaryLight = Color(0xFF8B85FF);
   static const Color primaryDark = Color(0xFF5046E5);
 
-  // Secondary
   static const Color secondary = Color(0xFF00BFA6);
   static const Color secondaryLight = Color(0xFF33CBB5);
   static const Color secondaryDark = Color(0xFF00A38D);
 
-  // Accent
   static const Color accent = Color(0xFFFF6584);
   static const Color accentLight = Color(0xFFFF8BA0);
 
-  // Semantic
   static const Color success = Color(0xFF00B894);
   static const Color warning = Color(0xFFF39C12);
   static const Color error = Color(0xFFE74C3C);
   static const Color info = Color(0xFF3498DB);
 
-  // Dark Theme
+  static const Color infoColor = info;
+  static const Color successColor = success;
+  static const Color warningColor = warning;
+  static const Color errorColor = error;
+
   static const Color darkBackground = Color(0xFF1A1A2E);
   static const Color darkSurface = Color(0xFF16213E);
   static const Color darkCard = Color(0xFF0F3460);
   static const Color darkText = Color(0xFFF8F9FA);
   static const Color darkTextMuted = Color(0xFF95A5A6);
 
-  // Light Theme
   static const Color lightBackground = Color(0xFFF8F9FA);
   static const Color lightSurface = Color(0xFFFFFFFF);
   static const Color lightCard = Color(0xFFFFFFFF);
   static const Color lightText = Color(0xFF2D3436);
   static const Color lightTextMuted = Color(0xFF636E72);
 
-  // Gradients
+  static Color get background => lightBackground;
+  static Color get surface => lightSurface;
+  static Color get card => lightCard;
+  static Color get text => lightText;
+  static Color get textMuted => lightTextMuted;
+
   static const LinearGradient primaryGradient = LinearGradient(
     colors: [primary, primaryLight],
     begin: Alignment.topLeft,

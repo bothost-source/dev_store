236a7737598fcef0224994eb31c04b7cecaa0fc6
bothost-source <destinyjob493@gmail.com>
2026-07-23 00:19:58 +0100

    Update main_screen.dart

--- lib/presentation/screens/public/main_screen.dart
+++ lib/presentation/screens/public/main_screen.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import '../../../core/services/auth_service.dart';
 import '../../bloc/auth_bloc.dart';
 import 'home_screen.dart';
 import 'search_screen.dart';
 import 'downloads_screen.dart';
 import 'settings_screen.dart';
-import '../developer/developer_dashboard_screen.dart';
-import '../admin/admin_dashboard_screen.dart';
 import '../../widgets/bottom_nav_bar.dart';
 
 class MainScreen extends StatefulWidget {
@@ -20,35 +17,25 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
 
+  final screens = [
+    const HomeScreen(),
+    const SearchScreen(),
+    const DownloadsScreen(),
+    const SettingsScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, state) {
-        final isAdmin = state is Authenticated && state.user.isAdmin;
-        final isDeveloper = state is Authenticated && state.user.isDeveloper;
-
-        final screens = [
-          const HomeScreen(),
-          const SearchScreen(),
-          const DownloadsScreen(),
-          if (isDeveloper) const DeveloperDashboardScreen(),
-          if (isAdmin) const AdminDashboardScreen(),
-          const SettingsScreen(),
-        ];
-
-        return Scaffold(
-          body: IndexedStack(
-            index: _currentIndex,
-            children: screens,
-          ),
-          bottomNavigationBar: BottomNavBar(
-            currentIndex: _currentIndex,
-            onTap: (index) => setState(() => _currentIndex = index),
-            isDeveloper: isDeveloper,
-            isAdmin: isAdmin,
-          ),
-        );
-      },
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: IndexedStack(
+        index: _currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+      ),
     );
   }
 }

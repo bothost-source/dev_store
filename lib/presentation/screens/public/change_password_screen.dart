e1d3954babf8f65b79e1326ff584effcc9b5ecd8
bothost-source <destinyjob493@gmail.com>
2026-07-23 01:58:50 +0100

    Update change_password_screen.dart

--- lib/presentation/screens/public/change_password_screen.dart
+++ lib/presentation/screens/public/change_password_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../core/services/auth_service.dart';
+import '../../../core/services/auth_service.dart';
 
 class ChangePasswordScreen extends StatefulWidget {
   const ChangePasswordScreen({super.key});
